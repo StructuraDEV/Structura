516568d98e9ad2be51bd977e673288e315a4caaa
StructuraDEV <structurai@proton.me>
2025-01-05 18:16:30 -0500

    Update state.rs

--- programs/structura_program/src/state.rs
+++ programs/structura_program/src/state.rs
@@ -1,1 +1,15 @@
+use anchor_lang::prelude::*;
 
+#[account]
+pub struct ProjectAccount {
+    pub name: String,
+    pub budget: u64,
+    pub milestones_completed: u8,
+}
+
+impl ProjectAccount {
+    pub const LEN: usize = 8  // Discriminator
+        + 32                  // name (up to 32 bytes in this example)
+        + 8                   // budget (u64)
+        + 1;                  // milestones_completed (u8)
+}
