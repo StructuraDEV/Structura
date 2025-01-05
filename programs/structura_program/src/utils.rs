89ccfc7e600b9d2375178884776908dcc32bbd0b
StructuraDEV <structurai@proton.me>
2025-01-05 18:36:51 -0500

    Update utils.rs

--- programs/structura_program/src/utils.rs
+++ programs/structura_program/src/utils.rs
@@ -1,8 +1,5 @@
 use anchor_lang::prelude::*;
 
-// Placeholder for utility functions.
-
 pub fn check_something() -> Result<()> {
-    // Example: ensure user is allowed to do an action
     Ok(())
 }
